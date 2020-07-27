b23847c893e9363d943f4c2fe14512ed37f0d599
jenk1 <19733552+jenk1@users.noreply.github.com>
2020-07-27 19:19:32 -0400

    Extended scope of make_graph and added docs

--- module_visualizer/package_vis.py
+++ module_visualizer/package_vis.py
@@ -1,3 +1,4 @@
+from helper import gather_nodes
 import networkx as nx
 import matplotlib.pyplot as plt
 from os import walk
@@ -8,10 +9,19 @@ rcParams['figure.figsize'] = 26, 18
 
 
 def fill_graph(node_list, file, graph):
-    """Takes a list of nodes and adds them to the network graph"""
+    """Takes a list of nodes and adds them to the network graph
+
+    Args:
+        node_list: list of nodes
+        file: name of python file
+        graph: networkx graph
+
+    Returns:
+        Network graph
+    """
 
     for i in node_list:
-        if('.' in i):
+        if '.' in i:
             temp = i.split('.')
             graph.add_edge(file[:-3], temp[0])
             for j in range(0, len(temp)-1):
@@ -21,7 +31,14 @@ def fill_graph(node_list, file, graph):
 
 
 def get_files(path):
-    """Gathers all python files from a folder"""
+    """Gathers all python files from a folder
+
+    Args:
+        path (str): path to desired folder
+
+    Returns:
+        Dictionary with python file as key and the path to it as the value
+    """
 
     # Set up dict with file as key and path as value
     name_path_dict = {}
@@ -34,14 +51,25 @@ def get_files(path):
     return name_path_dict
 
 
-def make_graph():
-    """Makes the network graph"""
+def make_graph(path):
+    """Make the network graph
+    
+    Args:
+        path (str): path to desired folder
+
+    Returns:
+        Network graph
+    """
 
     # make the graph
-    G = nx.DiGraph()
+    g = nx.DiGraph()
+
+    name_path_dict = get_files(path)
 
     # now check to see if files in dictionary keys are .py files
     for file in name_path_dict.keys():
-        if(file.lstrip().rstrip()[-3:] == '.py' and file != '__init__.py'):
+        if file.lstrip().rstrip()[-3:] == '.py' and file != '__init__.py':
             node_list = gather_nodes(name_path_dict[file] + '\\' + file)
-            fill_graph(node_list, file, G)
+            fill_graph(node_list, file, g)
+
+    return g
