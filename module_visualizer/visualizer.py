5b02efa5392f5c6826874f58803f8c44eb4ef617
jenk1 <19733552+jenk1@users.noreply.github.com>
2020-12-01 21:47:10 -0500

    brought in gather_nodes and clean_import methods

--- module_visualizer/visualizer.py
+++ module_visualizer/visualizer.py
@@ -1,5 +1,7 @@
 from os import walk
 
+# C:\Users\Michael\Desktop\desktop0\Code_Project\module_visualizer\module_visualizer
+
 
 class GraphVisualizer:
 
@@ -37,10 +39,93 @@ class GraphVisualizer:
                 if file.lstrip().rstrip()[-3:] == '.py' and file != '__init__.py':
                     self.name_path_dict[file] = dirpath
 
-    def gather_nodes(self):
-        pass
+    def gather_nodes(self, filename):
+        """Gathers all imports that will be nodes for the graph
 
-    def clean_imports(self):
+        Takes a file as input and from there processes the file so that the
+        output is a list of all the possible nodes as well as their paths. An
+        example of this would be having numpy and linspace as the two nodes in
+        the graph and it would be displayed in the list as numpy.linspace
+
+        Args:
+            filename (str): The filename
+
+        Returns:
+            A list of strings that contains all the nodes for the network graph
+        """
+
+        # process the file
+        with open(filename) as f:
+            content = f.readlines()
+
+        # remove any extra whitespace from both ends
+        content = [i.lstrip().rstrip() for i in content]
+
+        # gather just the import lines
+        all_imports = [line for line in content if line[0:6] == 'import' or
+                       line[0:4] == 'from']
+        print(all_imports)
+        print()
+
+        list_of_nodes = self.clean_imports(all_imports)
+
+        return list_of_nodes
+
+    def clean_imports(self, import_list):
+        """Puts all imports in a standard format
+
+        If import is "import a, b, c" then method will put a, b, and c in
+        separate entries. If import is "from a import b" then method will
+        put a.b into the list. This is also true for "from _ as _". Also
+        removes "as _" at the end of imports so there is no ambiguity.
+
+        Args:
+            import_list (str): A list of strings from python file 
+
+        Returns:
+            A list of strings that have the imports in standard format
+
+        """
+
+        mod_lst = []
+
+        while import_list:
+            if ',' in import_list[0]:
+                if import_list[0][0:6] == 'import':
+                    temp_ = import_list[0][6:].split(',')
+                    for i in temp_:
+                        import_list.append('import ' + i)
+
+                    import_list[0] = 'Fixed'
+
+                if import_list[0][0:4] == 'from':
+                    temp_ = import_list[0].split('import')
+                    part_1 = temp_[0]
+                    part_2 = temp_[1].lstrip().rstrip().split(',')
+                    for i in part_2:
+                        import_list.append(str(part_1) + ' import ' + str(i))
+
+                    import_list[0] = 'Fixed'
+
+            temp = import_list[0].split()
+
+            if len(temp) == 2 and temp[0] == 'import':
+                mod_lst.append(temp[1])
+                import_list[0] = 'Fixed'
+            elif len(temp) == 4 and temp[0] == 'import' and temp[2] == 'as':
+                mod_lst.append(temp[1])
+                import_list[0] = 'Fixed'
+            elif len(temp) == 4 and temp[0] == 'from':
+                if temp[3] != '*':
+                    mod_lst.append(str(temp[1]) + '.' + str(temp[3]))
+                else:
+                    mod_lst.append(str(temp[1]))
+                import_list[0] = 'Fixed'
+
+            del import_list[0]
+        return mod_lst
+
+    def gather_all_nodes(self):
         pass
 
 
@@ -52,6 +137,10 @@ def main():
     print(a.name_path_dict)
     print()
     print(a.name_path_dict.keys())
+    print()
+    print()
+    print(a.gather_nodes(a.name_path_dict['helper.py'] + '//' + 'helper.py'))
+    print()
 
 
 if __name__ == '__main__':
